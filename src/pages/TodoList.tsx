76327f2ca70ad18b2d30b1e5c65a759d5bb30331
t10o <dev@t10o.one>
2023-04-22 13:31:46 +0900

    :sparkles: フローティングボタンを追加

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,15 +1,27 @@
-import { FlatList } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+import { FlatList, Icon, IconButton } from 'native-base'
 
 import { TODO_LIST } from '../dummy-data/todo-list'
 import { TodoListItem } from '../features/TodoList/TodoListItem'
 
 export const TodoList = (): JSX.Element => {
   return (
-    <FlatList
-      data={TODO_LIST}
-      renderItem={({ item }) => (
-        <TodoListItem label={item.label} isFinished={item.isFinished} />
-      )}
-    />
+    <>
+      <FlatList
+        data={TODO_LIST}
+        renderItem={({ item }) => (
+          <TodoListItem label={item.label} isFinished={item.isFinished} />
+        )}
+      />
+
+      <IconButton
+        position="absolute"
+        right="50%"
+        bottom={3}
+        backgroundColor="blue.500"
+        borderRadius="full"
+        icon={<Icon size="2xl" as={MaterialIcons} name="add" color="white" />}
+      />
+    </>
   )
 }
